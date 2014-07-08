27147260a67a86717871e96df9b9f8195276f346
Allen Kleiner <akleiner24@gmail.com>
2014-07-07 19:17:15 -0500

    basic webpage and scripts

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+/*
+ * Just a very basic script to add visual effects to elements.
+ */
+
+$(document).ready(function(){
+	$('h1').hide().fadeIn("slow");
+	$('p').css("opacity", 0).delay(500).animate({opacity: 1});
+})
